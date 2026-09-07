71d073523cfc4bd18e4ef8c600f02e306a7e799e
Pete Petrash <pete.petrash@gmail.com>
2026-09-06 19:09:36 -0700

    Point website guides at the current API

--- apps/lab/src/components/sceneCatalog.ts
+++ apps/lab/src/components/sceneCatalog.ts
@@ -2,7 +2,7 @@
 // The source reference matches the development API shown on this site.
 
 export const SOURCE_ROOT = 'https://github.com/petekp/munari'
-export const SOURCE_REF = 'c9dc2f359ca253b9031b60e079f5ff2272053fdd'
+export const SOURCE_REF = 'dae999447d0931d35b6490ae4d1ebcdbbf08a15c'
 export const GUIDE_URL = `${SOURCE_ROOT}/blob/${SOURCE_REF}/README.md#your-first-surface`
 export const SOURCE_ARCHIVE = `${SOURCE_ROOT}/archive/${SOURCE_REF}.zip`
 export const BROWSER_GUIDE = 'https://developer.chrome.com/blog/html-in-canvas-origin-trial'
